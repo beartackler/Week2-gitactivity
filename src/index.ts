e3e8feb58af34070254560dcea0ea749041e8c7b
Timur Monasypov <timurmonasypov@Timurs-MacBook-Pro.local>
2024-09-15 19:29:50 -0400

    implement multiply op

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,10 @@ const calc = new Calculator();
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 
-const result_subtract = calc.subtract(5, 3);
+const result_subtract = calc.subtract(6, 3);
 console.log(`result_subtract = ${result_subtract}`);
+
+const result_multiply = calc.multiply(6, 3);
+console.log(`result_multiply = ${result_multiply}`);
+
+//test
